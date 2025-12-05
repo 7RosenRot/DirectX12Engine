6562e96737174afb0b2a16d328f2fe66760c02fb
Даниил Шеметов <lego.den.2005@mail.ru>
2025-12-05 13:18:19 +0300

    new class: DirectX12Graphics

---
+++ DX3D12Engine/Include/Graphics/DirectX12Graphics.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <Include/Window/Window.hpp>
+
+namespace D3D12Engine {
+    class DirectX12Graphics {
+    public:
+        DirectX12Graphics();
+        virtual ~DirectX12Graphics();
+    protected:
+    };
+}
---
+++ DX3D12Engine/Source/Graphics/DirectX12Graphics.cpp
@@ -0,0 +1,5 @@
+#include <Include/Graphics/DirectX12Graphics.hpp>
+
+D3D12Engine::DirectX12Graphics::DirectX12Graphics() {}
+
+D3D12Engine::DirectX12Graphics::~DirectX12Graphics() {}
